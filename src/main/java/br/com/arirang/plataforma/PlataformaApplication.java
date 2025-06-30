5b228271823a0f4ccc9426d7cec0bfe955af8af8
murilo melo <murilomelomd@gmail.com>
2025-06-29 22:05:48 -0300

    primeiro commit da plataforma arirang

---
+++ src/main/java/br/com/arirang/plataforma/PlataformaApplication.java
@@ -0,0 +1,13 @@
+package br.com.arirang.plataforma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaApplication.class, args);
+	}
+
+}
