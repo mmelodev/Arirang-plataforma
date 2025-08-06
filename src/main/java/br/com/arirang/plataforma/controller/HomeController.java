b9d7e13baeb6dde0ac6bf2c0d29bf2a6c50a2853
Murilo Melo <murilomelomd@gmail.com>
2025-08-06 13:57:32 -0300

    finalizando primeira versao da home.html e integrando ao thymeleaf

---
+++ src/main/java/br/com/arirang/plataforma/controller/HomeController.java
@@ -0,0 +1,12 @@
+package br.com.arirang.plataforma.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home"; // Retorna o template home.html
+    }
+}
